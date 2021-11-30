af7b48cafe9966b00864d6909e1791f25d722cae
Miguel Fernando garcia Salazar <migeru_garcia@hotmail.com>
2021-11-30 10:39:20 -0600

    none

---
+++ FuncResult.Serilog/SerilogConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+using FuncResult.Logger;
+using Serilog;
+
+namespace FuncResult.Serilog
+{
+    //public static  class SerilogConfiguration
+    //{
+    //    public static void Build()
+    //    {
+    //        Log.Logger = new LoggerConfiguration()
+    //                     .WriteTo.Console()
+    //                     .CreateLogger();
+    //    }
+    //}
+
+    
+}
